2b8f55ca6f2f5cd7b8ce951b13a82dc0739cd7c5
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-02-26 15:43:22 +0000

    structure

---
+++ src/app/components/Context/urls.ts
@@ -0,0 +1,32 @@
+export const urls = [
+  {
+    url: "https://e360.yale.edu/digest/indonesia-malaysia-deforestation",
+    title: "Indonesia Deforestation",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://cleantechnica.com/2023/06/29/solar-82-of-power-capacity-growth-in-india-in-2022/",
+    title: "Solar Power in India",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/",
+    title: "Matisee Thybulle",
+    seeded: false,
+    loading: false,
+  },
+  {
+    url: "https://open.spotify.com/artist/55WGXEp1qUerac8ChlL5Ii?si=KtXN9rvHThK7ugLRC21rVw",
+    title: "Circumference Spotify",
+    seeded: false,
+    loaded: false,
+  },
+  {
+    url: "https://open.spotify.com/artist/1WNoKxsp715jez1Td4vthc?si=rvfR5lpGSKisDpYpU6S6Pw",
+    title: "Electric Callboy Spotify",
+    seeded: true,
+    loaded: false,
+  },
+];
